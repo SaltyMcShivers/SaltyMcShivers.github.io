d68578e854591a38c9ce439984958570b1dec466
Michael Andryauskas <SaltyMcShivers@bitbucket.org>
2015-12-07 14:28:26 -0500

    Site Files Take 2

---
+++ JavascriptSource.js
@@ -0,0 +1,35 @@
+function replaceImage(targetID, newImg){
+	document.getElementById(targetID).src = newImg;
+}
+
+function scroll_if_anchor(href) {
+    href = typeof(href) == "string" ? href : $(this).attr("href");
+    
+    // You could easily calculate this dynamically if you prefer
+    var fromTop = 100;
+    
+    // If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+    // Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+    if(href.indexOf("#") == 0) {
+        var $target = $(href);
+        
+        // Older browser without pushState might flicker here, as they momentarily
+        // jump to the wrong position (IE < 10)
+        if($target.length) {
+            $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+            if(history && "pushState" in history) {
+                history.pushState({}, document.title, window.location.pathname + href);
+                return false;
+            }
+        }
+    }
+}    
+
+// When our page loads, check to see if it contains and anchor
+
+$(function(){
+	
+	$(".menuButton a").click(function(){
+		scroll_if_anchor($(this).href);
+	});
+});
